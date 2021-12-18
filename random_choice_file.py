e398c4f928a4a8ee494e4533eb4c806dc2884008
Dan Sato <s19C1054bu@s.chibakoudai.jp>
2021-12-18 11:18:12 +0900

    divide class

--- random_choice_file.py
+++ random_choice_file.py
@@ -5,45 +5,49 @@ import numpy as np
 from PIL import Image
 import random
 import glob
+import datetime
 
-def random_choice(datas, num_images, len_images):
-	"""
-	image_list = np.zeros((0, 3, 480, 640), dtype=np.float32)
-	for i in range(int(num_images)):
-		random_number = random.uniform(0, len_images)
-		#choice_image = cv2.imread(datas[int(random_number)])
-		choice_image = np.array(Image.open(datas[int(random_number)]))
-		image_list.append(choice_image)
-		return image_list
-	"""
-	os.mkdir('pickup_files')
-	for i in range(int(num_images)):
-		random_number = random.uniform(0, len_images)
-		choice_image = cv2.imread(datas[int(random_number)])
-		print(datas[int(random_number)])
-		file_name = os.path.split(datas[int(random_number)])
-		print(file_name[0])
-		print(os.path.join(file_name[0],'pickup_files',file_name[-1]))
-		#cv2.imwrite(os.path.join(file_name[0],'pickup_files',file_name[-1]), choice_image)
+class random_pickup:
+	def __init__(self, data_dir):
+		self.dt = datetime.datetime.now().strftime('%y%m%d')
+		if os.path.exists(data_dir):
+			self.dt_dir = os.path.join(data_dir, self.dt + '-pickup')
+			print(self.dt_dir)
+			os.mkdir(os.path.join(data_dir, self.dt))
 
-def load_data(data_dir):
-	if not os.path.exists(data_dir):
-		print(f"Not exists 『{data_dir}』 !!!")
-		sys.exit()
-	datas = glob.glob(data_dir + '/*.jpg')
-	return datas
+		else:
+			print(f"Not exists 『{data_dir}』 !!!")
+			sys.exit()
 
-def main ():
+	def random_choice(self, datas, num_images, len_images):
+		for i in range(int(num_images)):
+			self.random_number = random.uniform(0, len_images)
+			choice_image = cv2.imread(datas[int(self.random_number)])
+			print(datas[int(self.random_number)])
+			file_name = os.path.split(datas[int(self.random_number)])
+			print(file_name[0])
+			print(os.path.join(file_name[0], self.dt_dir, file_name[-1]))
+			#cv2.imwrite(os.path.join(file_name[0],'pickup_files',file_name[-1]), choice_image)
+
+	def load_data(self, data_dir):
+		self.datas = glob.glob(data_dir + '/*.jpg')
+		return self.datas
+
+def main():
 	print("data_dir : ",end="")
 	data_dir = input()
-	images = load_data(data_dir)
+
+	rp = random_pickup(data_dir)
+
+	images = rp.load_data(data_dir)
 	print(images)
+	
 	print("How many images do you want? : ",end="")
 	num_image = input()
 	len_datas = len(images)
 	print(len_datas)
-	a = random_choice(images, num_image, len_datas)
-	#print(a[1])
+	
+	rp.random_choice(images, num_image, len_datas)
 
 if __name__ == '__main__':
 	main()
